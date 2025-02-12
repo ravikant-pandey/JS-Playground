92dea7c44a402eee9fbea950b95d9389b69d067b
Pandey404 <howtoearn554@gmail.com>
2025-02-12 16:10:58 +0000

    variables use

--- 01_basic/variables.js
+++ 01_basic/variables.js
@@ -1,4 +1,17 @@
-const number1 = 90;
-const number2 = 45;
+// const number1 = 45;
+// const number2 = 45;
 
-console.log(number1+number2);
+// console.log(number1+number2);
+
+const accountId = "Rahul123";
+var emailId = "rahul@example.com";
+let accountPassword = "123@abc";
+userCity = "Bijnore";
+
+// accountId = 2313; // Change not allowed
+console.log(accountId);
+emailId = "rahul123@example.com";
+accountPassword = "2345rtyu";
+userCity = "MBD";
+let accountState;
+console.table([emailId,accountPassword,userCity,accountState]);
