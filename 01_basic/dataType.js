9d6dcd8e6df59b228d8542c62d4b590660a0e679
Pandey404 <howtoearn554@gmail.com>
2025-02-14 10:36:40 +0000

    why string to number

--- 01_basic/dataType.js
+++ 01_basic/dataType.js
@@ -15,3 +15,18 @@ let state;
 // object
 
 console.log(typeof(isLoggedin));
+
+// console.log(2 + 2);
+// console.log(2 - 2);
+// console.log(2 * 2);
+// console.log(2 ** 3);
+// console.log(2 / 2);
+// console.log(3 % 2);
+
+// console.log("1" + 2);
+// console.log(1 + "3");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "4");
+// console.log(3 + ((4 * 3) % 2));
+
+console.log(true);
