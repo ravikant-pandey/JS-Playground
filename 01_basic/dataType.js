2ed77a4572b40f7e2e2da080f10fc6dcef5f96a0
Pandey404 <howtoearn554@gmail.com>
2025-02-14 02:41:01 +0000

    some data types and details

--- 01_basic/dataType.js
+++ 01_basic/dataType.js
@@ -1,3 +1,17 @@
 "Use strict"; // Treat all js code in newer version.
 
 // alert("Hello",5+4); We are using node.js not browser so it is not execute.
+let isLoggedin = false;
+let state;
+
+// number => 2 to power 53;
+// BigInt;
+// String => "";
+// Boolean => true/false;
+// null => standalone value; It is a object 
+// undefined it is type of undefine
+// Symbol => unique value
+
+// object
+
+console.log(typeof(isLoggedin));
