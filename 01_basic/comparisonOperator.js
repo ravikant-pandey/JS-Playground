b6d5ad0aa525df6c2128052e47723950f2d7a30f
Pandey404 <howtoearn554@gmail.com>
2025-02-14 10:54:28 +0000

    comparison Operator

---
+++ 01_basic/comparisonOperator.js
@@ -0,0 +1,18 @@
+// Comparison operator
+
+// console.log(2>3);
+// console.log(2<3);
+// console.log(2==2);
+// console.log(5>=8);
+// console.log(3<=3);
+// console.log(2!=4);
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// console.log(undefined == 0);
+// console.log(undefined>0);
+// console.log(undefined<0);
+
+console.log("2"===2); // check data Type also value
