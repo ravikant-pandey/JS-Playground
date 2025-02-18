5400d89f7f89044fd24da08f672479e166607e0b
Pandey404 <howtoearn554@gmail.com>
2025-02-18 17:18:22 +0000

    if else statement

---
+++ control_flow/02.js
@@ -0,0 +1,16 @@
+// else if
+balance = 880; // Change this value to test different cases
+
+let balance = 450; // Change this value to test different cases
+
+if (balance < 500) {
+  console.log("Balance is less than 500");
+} else if (balance < 750) {
+  console.log("Balance is less than 750 but greater than or equal to 500");
+} else if (balance < 900) {
+  console.log("Balance is less than 900 but greater than or equal to 750");
+} else if (balance < 1000) {
+  console.log("Balance is less than 1000 but greater than or equal to 900");
+} else {
+  console.log("Balance is 1000 or more");
+}
