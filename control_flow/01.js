1b51392b2ef1b4c799453ba75368a2a300dbdc3c
Pandey404 <howtoearn554@gmail.com>
2025-02-18 17:17:51 +0000

    if and else statement

---
+++ control_flow/01.js
@@ -0,0 +1,50 @@
+// If
+// else
+// <, >, =, <=, >=, ==, ===, !=, !==
+
+
+// const isUeserLoggedIn = true;
+// if (isUeserLoggedIn == true) {
+//   console.log("Logged in");
+// }
+
+
+// if (2 === "2") {
+//   console.log("Executed...");
+// } else {
+//   console.log("Not executed");
+// }
+
+
+// let temperature = 30; // Change this value to test different temperatures
+
+// if (temperature > 30) {
+//     console.log("It's too hot outside! Stay hydrated.");
+// } else {
+//     console.log("The weather is normal. Enjoy your day!");
+// }
+
+// // Scope
+// const score = 400;
+// if(score===400)
+// {
+//   let power = "Run Fast";
+//   console.log(`power is: ${power}`);
+// }
+// console.log(`power is: ${power}`);
+
+
+// Short hand
+// const balance = 4000;
+// if(balance==4000) console.log("Yes...");
+//   // ,console.log("Test work successfully"); do not use this type of code..
+
+  // let result = 2 == 2 ? "Yes....." : "Not";  
+  // if (result) {
+  //     console.log(result); // Output: Yes
+  // }
+
+  // console.log(2 == 2 ? "Yes" : "Not"); // Output: Yes
+
+  
+  
