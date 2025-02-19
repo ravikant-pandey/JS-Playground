063fab1c8eaab9c787ac7d9e41c35447ecc7b0b9
Pandey404 <howtoearn554@gmail.com>
2025-02-19 15:38:23 +0000

    learn about if else

--- control_flow/02.js
+++ control_flow/02.js
@@ -1,16 +1,30 @@
 // else if
-balance = 880; // Change this value to test different cases
+// let balance = 450; // Change this value to test different cases
 
-let balance = 450; // Change this value to test different cases
+// if (balance < 500) {
+//   console.log("Balance is less than 500");
+// } else if (balance < 750) {
+//   console.log("Balance is less than 750 but greater than or equal to 500");
+// } else if (balance < 900) {
+//   console.log("Balance is less than 900 but greater than or equal to 750");
+// } else if (balance < 1000) {
+//   console.log("Balance is less than 1000 but greater than or equal to 900");
+// } else {
+//   console.log("Balance is 1000 or more");
+// }
 
-if (balance < 500) {
-  console.log("Balance is less than 500");
-} else if (balance < 750) {
-  console.log("Balance is less than 750 but greater than or equal to 500");
-} else if (balance < 900) {
-  console.log("Balance is less than 900 but greater than or equal to 750");
-} else if (balance < 1000) {
-  console.log("Balance is less than 1000 but greater than or equal to 900");
-} else {
-  console.log("Balance is 1000 or more");
+const userLoggedIn = true;
+const debitCard = true;
+const panIndia = false;
+const loggedInFromGoogle = false;
+const loggedInFromGmail = true;
+
+if(userLoggedIn && debitCard && panIndia)
+{
+  console.log("Allow to buy a course...")
 }
+if(loggedInFromGoogle || loggedInFromGmail)
+{
+  console.log("Yes you can log in...");
+}
+
