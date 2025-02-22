1636605ec12430d7d3894c188a2b9c4f86548b27
Pandey404 <howtoearn554@gmail.com>
2025-02-22 06:58:28 +0000

    discuss map

---
+++ Loops/07.js
@@ -0,0 +1,13 @@
+// Map
+
+const myArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const plusTen = myArray.map((num)=>
+{
+    return num + 10;
+});
+console.log(plusTen);
+
+// // Short hand
+// const plusTen = myArray.map((num) => num + 10);
+// console.log(plusTen);
+
